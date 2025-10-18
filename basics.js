86a86c2123d1dff14eadaca8ab22715a4b0f8a23
Dok112 <dokzzz112@gmail.com>
2025-10-18 15:57:19 +0300

    Day 2 — Функции и параметры завершены

--- basics.js
+++ basics.js
@@ -5,3 +5,18 @@ const height = 40;
 console.log("Длина:", length);
 console.log("Ширина:", width);
 console.log("Высота:", height);
+
+function  calculateVolume(length, width, height) {
+  return  length * width * height;
+}
+
+console.log("Объем 1", calculateVolume(5, 2, 3));
+console.log("Объем 2", calculateVolume(10, 4, 8));
+console.log("Объем 2", calculateVolume(length, width, height));
+
+function checkEvenOdd(x) {
+  return x % 2 ===0;
+}
+
+console.log(checkEvenOdd(13));
+
